7f35fcdf7c545e92f87d53630f48134783cbe861
Your Name <maherin2114@gmail.com>
2026-06-27 10:57:47 +0530

    nested if-else example

--- 11. conditional_statements.py
+++ 11. conditional_statements.py
@@ -24,3 +24,56 @@ elif a==18:
     print("you can drive but be careful")
 else:
     print("you can drive")
+<<<<<<< HEAD
+=======
+
+# elif acts as else if statement, it is used to check multiple conditions. It is optional, you can use as many elif statements as you want. But you can only use one else statement. The else statement is used to execute a block of code when all the conditions are false.
+
+if a<18:
+    print("you cannot drive")
+elif a==18:
+    print("you can drive but be careful")   
+else:
+    print("you can drive")
+
+
+# cleaned up version
+wt = int(input("Enter your weight in kg: "))
+ht = float(input("Enter your height in metres: "))
+
+bmi = round(wt / (ht ** 2), 1)
+print(f"Your BMI is: {bmi}")
+
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi < 25:
+    print("You are normal weight")
+elif bmi < 30:
+    print("You are overweight")
+else:
+    print("You are obese")
+
+
+# nested if else statements
+age = int(input("Enter your age: "))
+weekend = input("Is it weekend? (yes/no): ")
+if age < 12:
+    if weekend == "yes":
+        print("Ticket price is 100")
+    else:
+        print("Ticket price is 70")
+
+elif age <= 59:
+    if weekend == "yes":
+        print("Ticket price is 200")
+    else:
+        print("Ticket price is 150")
+
+else:
+    if weekend == "yes":
+        print("Ticket price is 100")
+    else:
+        print("Ticket price is 80")
+
+print("YAYYY!!!")
+>>>>>>> 69fe127 (nested if-else example)
